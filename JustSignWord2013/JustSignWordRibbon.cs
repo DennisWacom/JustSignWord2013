ab8ffeb3653d819eed58974e7e1323d95228d72d
Dennis Cai <dennis.cai@wacom.com.sg>
2017-03-30 11:12:51 +0800

    first commit

---
+++ JustSignWord2013/JustSignWordRibbon.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+using System.Drawing;
+using System.Resources;
+using FLSIGCTLLib;
+using FlSigCaptLib;
+using System.Windows.Forms;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new JustSignWordRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace JustSignWord2013
+{
+    [ComVisible(true)]
+    public class JustSignWordRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public JustSignWordRibbon()
+        {
+        }
+
+        public Bitmap getSignatureIcon(Office.IRibbonControl control)
+        {
+            return JustSignWord2013.Properties.Resources.sign;
+        }
+
+        public void CaptureSignature(Office.IRibbonControl control)
+        {
+            sign();
+        }
+
+        public void sign()
+        {
+            SigCtl sigCtl = new SigCtl();
+            DynamicCapture dc = new DynamicCapture();
+            DynamicCaptureResult res = dc.Capture(sigCtl, "Name", "Reason", null, null);
+
+            if (res == DynamicCaptureResult.DynCaptOK)
+            {
+                SigObj sigObj = (SigObj)sigCtl.Signature;
+                //sigObj.set_ExtraData("AdditionalData", "C# test: Additional data");
+
+                String filename = System.IO.Path.GetTempFileName();
+                try
+                {
+                    sigObj.RenderBitmap(filename, 400, 200, "image/png", 0.7f, 0x000000, 0xffffff, 10.0f, 10.0f, RBFlags.RenderOutputFilename | RBFlags.RenderColor32BPP | RBFlags.RenderEncodeData | RBFlags.RenderBackgroundTransparent);
+
+                    Globals.ThisAddIn.Application.Selection.Range.InlineShapes.AddPicture(filename);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("JustSignWord2013.JustSignWordRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# JustSignWord2013
+
+This is Word 2013 plugin (also support Word 2015), to use Wacom Signature SDK to capture signatures in Word.
+
+## Usage
+
+After installing, there should be a "Add-ins" tab in the ribbon, and a "Capture" button available to capture signatures.
+First, make sure a word document is opened, and the word cursor is at the position where you want the signature to appear. Click on the Capture button,  a signature pop up will appear to capture your signature from a Wacom device. After clicking the "OK" button, the signature will appear in the document where your cursor is located.
+
+## Prerequisite
+Wacom Signature SDK
